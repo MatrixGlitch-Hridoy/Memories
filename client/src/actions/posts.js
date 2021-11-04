9cca4f2e3f94de53c5c09c898998629d2a52984d
Hridoy <rkhridoy68@gmail.com>
2021-11-05 01:48:42 +0600

    post a memory implemented

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -9,3 +9,12 @@ export const getPosts = () => async (dispatch) => {
         console.log(err.messsage);
     }
 }
+
+export const createPost = (post) => async (dispatch) => {
+    try{
+        const {data} = await api.createPost(post);
+        dispatch({type:'CREATE',payload:data});
+    }catch(err){
+        console.log(err.messsage);
+    }
+} 
