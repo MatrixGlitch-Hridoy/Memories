eedb508a5ee7a93b9e03743aab9e1a892c7a3c2c
Hridoy <rkhridoy68@gmail.com>
2021-11-07 00:08:09 +0600

    updated

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 const router = express.Router();
 
+//All Routes
 router.get('/',getPosts);
 router.post('/',createPost);
 router.patch('/:id',updatePost);
